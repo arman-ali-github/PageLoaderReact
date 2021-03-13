9904f49a9cd463a1f35b26167e6db5f1755d121c
arman-ali-github <80573676+arman-ali-github@users.noreply.github.com>
2021-03-13 15:10:57 +0530

    Initial commit

---
+++ src/Loader.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ShowDetail() {
+  return (
+    <div className="center spinner-box">
+      <div className="circle-border">
+        <div className="circle-core"></div>
+      </div>
+    </div>
+  );
+}
+
+export default ShowDetail;
